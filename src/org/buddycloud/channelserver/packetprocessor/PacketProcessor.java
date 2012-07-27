bf8e3874046b872f08325e998670b99b544a9d11
Lloyd Watkin <lloyd.watkin@surevine.com>
2012-07-27 11:35:21 +0100

    Converted to maven project

--- src/org/buddycloud/channelserver/packetprocessor/PacketProcessor.java
+++
@@ -1,9 +0,0 @@
-package org.buddycloud.channelserver.packetprocessor;
-
-import org.xmpp.packet.Packet;
-
-public interface PacketProcessor<T extends Packet> {
-    
-    public void process(T packet) throws Exception;
-    
-}

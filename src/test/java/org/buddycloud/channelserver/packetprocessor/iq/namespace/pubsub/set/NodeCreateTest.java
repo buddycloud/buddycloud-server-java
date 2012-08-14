a630d2ccb5fbf4197d1ae5ad2405e312f295a78b
Lloyd Watkin <lloyd.watkin@surevine.com>
2012-08-14 16:16:33 +0100

    Working test for missing node

--- src/test/java/org/buddycloud/channelserver/packetprocessor/iq/namespace/pubsub/set/NodeCreateTest.java
+++ src/test/java/org/buddycloud/channelserver/packetprocessor/iq/namespace/pubsub/set/NodeCreateTest.java
@@ -2,25 +2,33 @@ package org.buddycloud.channelserver.packetprocessor.iq.namespace.pubsub.set;
 
 import static org.junit.Assert.fail;
 import org.dom4j.Element;
+import org.dom4j.dom.DOMElement;
 import org.dom4j.tree.BaseElement;
 
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
 
 
 import org.buddycloud.channelserver.db.DataStore;
 import org.buddycloud.channelserver.db.mock.Mock;
+import org.buddycloud.channelserver.packetHandler.iq.IQHandlerTest;
+import org.buddycloud.channelserver.packetprocessor.iq.namespace.pubsub.JabberPubsub;
 import org.buddycloud.channelserver.packetprocessor.iq.namespace.pubsub.set.NodeCreate;
 import org.junit.Before;
 import org.junit.Test;
+import org.xmpp.packet.JID;
 import org.xmpp.packet.Packet;
 import org.xmpp.packet.IQ;
+import org.xmpp.packet.PacketError;
 
-public class NodeCreateTest
+public class NodeCreateTest extends IQHandlerTest
 {
 	private IQ         stanza;
 	private DataStore  dataStore;
 	private NodeCreate nodeCreate;
+	private JID        jid;
+	private IQ         request;
 	private BlockingQueue<Packet> queue = new LinkedBlockingQueue<Packet>();
 
 	@Before
@@ -29,13 +37,15 @@ public class NodeCreateTest
 		dataStore  = new Mock();
 		queue      = new LinkedBlockingQueue<Packet>();
 		nodeCreate = new NodeCreate(queue, dataStore);
+		jid        = new JID("juliet@capulet.lit");
+		request    = readStanzaAsIq("/iq/pubsub/channel/create/request.stanza");
+		
 	}
 
 	@Test
 	public void testPassingCreateAsElementNameReturnsTrue()
 	{
 		Element element = new BaseElement("create");
-		
 		assertTrue(nodeCreate.accept(element));
 	}
 	
@@ -44,5 +54,20 @@ public class NodeCreateTest
 	{
 		Element element = new BaseElement("not-create");
 		assertFalse(nodeCreate.accept(element));
+	}
+	
+	@Test
+	public void testPassingNoNodeResultsInErrorStanza() throws Exception
+	{
+		Element element = new BaseElement("create");
+		nodeCreate.process(element, jid, request, null);
+
+		    Packet response = queue.poll(1, TimeUnit.MILLISECONDS);
+		    PacketError error           = response.getError();
+
+		assertNotNull(error);
+		assertEquals(PacketError.Type.modify, error.getType());
+		assertEquals(PacketError.Condition.bad_request, error.getCondition());
+
 	}
 }

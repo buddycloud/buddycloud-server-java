ca12ed4271fbf0cb6bc29b2e836b59460bdb80eb
Ash Ward <ash@netality.com>
2012-08-15 17:57:00 +0100

    More nodestore WIP

--- src/main/java/org/buddycloud/channelserver/db/NodeStore.java
+++ src/main/java/org/buddycloud/channelserver/db/NodeStore.java
@@ -1,9 +1,12 @@
 package org.buddycloud.channelserver.db;
 
 import java.util.Map;
+import java.util.Set;
 
 import org.buddycloud.channelserver.pubsub.affiliation.Affiliations;
+import org.buddycloud.channelserver.pubsub.affiliation.NodeAffiliation;
 import org.buddycloud.channelserver.pubsub.model.Node;
+import org.buddycloud.channelserver.pubsub.subscription.NodeSubscription;
 import org.buddycloud.channelserver.pubsub.subscription.Subscriptions;
 import org.xmpp.packet.JID;
 
@@ -17,15 +20,47 @@ public interface NodeStore {
 	 * @param owner the node owner.
 	 * @param nodeID the node id.
 	 * @param nodeConf a configuration map for the node.
+	 * @throws NodeStoreException if the operation could not be completed.
 	 */
-	void createNode(JID owner, String nodeID, Map<String,String> nodeConf);
+	void createNode(JID owner, String nodeID, Map<String,String> nodeConf) throws NodeStoreException;
 	
-	Node fetchNode(String nodeID);
+	/**
+	 * Retrieves a node from the data store.
+	 * @param nodeID the node id
+	 * @return the Node, or <code>null</code> if the node does not exist.
+	 * @throws NodeStoreException if the operation could not be completed.
+	 */
+	Node fetchNode(String nodeID) throws NodeStoreException;
 	
-	boolean nodeExists(String nodeID);
+	/**
+	 * Determine whether a node exists within the data store.
+	 * @param nodeID the node id.
+	 * @return <code>true</code> if the node exists, <code>false</code> otherwise.
+	 * @throws NodeStoreException if the operation could not be completed.
+	 */
+	boolean nodeExists(String nodeID) throws NodeStoreException;
 	
-	void setUserAffiliation(String nodeID, JID user, Affiliations affiliation);
+	/**
+	 * Sets a user's affiliation with a node. If the user doesn't already have an affiliation with the node
+	 * then one will be added.
+	 * @param nodeID the node id.
+	 * @param user the user.
+	 * @param affiliation the type of affiliation.
+	 * @throws NodeStoreException if the operation could not be completed.
+	 */
+	void setUserAffiliation(String nodeID, JID user, Affiliations affiliation) throws NodeStoreException;
 	
-	void setUserSubscription(String nodeID, JID user, Subscriptions subscription, String subscriptionId);
+	/**
+	 * Sets a user's subscription to a node. If the user doesn't already have a subscription then one will be added.
+	 * @param nodeID the node id.
+	 * @param user the user's JID.
+	 * @param subscriptionId an id for the user's subscription (to allow a user to have multiple subscriptions to a single node).
+	 * @param subscription the type of subscription.
+	 * @throws NodeStoreException if the operation could not be completed.
+	 */
+	void setUserSubscription(String nodeID, JID user, String subscriptionId, Subscriptions subscription) throws NodeStoreException;
 	
+	NodeAffiliation getUserAfilliation(String nodeID, JID user);
+	
+	Set<NodeSubscription> getUserSubscriptions(String nodeID, JID user);
 }
---
+++ src/main/java/org/buddycloud/channelserver/db/NodeStoreException.java
@@ -0,0 +1,17 @@
+package org.buddycloud.channelserver.db;
+
+public class NodeStoreException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	public NodeStoreException(final String message) {
+		super(message);
+	}
+	
+	public NodeStoreException(final String message, final Throwable t) {
+		super(message, t);
+	}
+	
+	public NodeStoreException(final Throwable t) {
+		super(t);
+	}
+}

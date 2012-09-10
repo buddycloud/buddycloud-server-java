92b5576b62e2e47f3d41d90dbd9f7b03b00bb4df
Lloyd Watkin <lloyd.watkin@surevine.com>
2012-09-10 12:15:53 +0100

    Checking node provided/exists, user has permission to modify

--- src/main/java/org/buddycloud/channelserver/packetprocessor/iq/namespace/pubsub/set/SubscriptionEvent.java
+++ src/main/java/org/buddycloud/channelserver/packetprocessor/iq/namespace/pubsub/set/SubscriptionEvent.java
@@ -2,15 +2,25 @@ package org.buddycloud.channelserver.packetprocessor.iq.namespace.pubsub.set;
 
 import java.util.concurrent.BlockingQueue;
 
+import org.apache.log4j.Logger;
 import org.buddycloud.channelserver.db.DataStore;
+import org.buddycloud.channelserver.db.DataStoreException;
+import org.buddycloud.channelserver.db.jedis.NodeSubscriptionImpl;
+import org.buddycloud.channelserver.packetprocessor.iq.namespace.pubsub.JabberPubsub;
 import org.buddycloud.channelserver.packetprocessor.iq.namespace.pubsub.PubSubElementProcessorAbstract;
+import org.buddycloud.channelserver.pubsub.affiliation.Affiliations;
 import org.dom4j.Element;
+import org.dom4j.Namespace;
+import org.dom4j.dom.DOMElement;
 import org.xmpp.packet.IQ;
 import org.xmpp.packet.JID;
 import org.xmpp.packet.Packet;
+import org.xmpp.packet.PacketError;
 
 public class SubscriptionEvent extends PubSubElementProcessorAbstract
 {
+	private static final Logger LOGGER = Logger.getLogger(SubscriptionEvent.class);
+	
 	/**
 	 * Constructor
 	 * 
@@ -28,8 +38,71 @@ public class SubscriptionEvent extends PubSubElementProcessorAbstract
 	 */
 	public void process(Element elm, JID actorJID, IQ reqIQ, Element rsm)
 			throws Exception {
-		// TODO Auto-generated method stub
-		
+    	element  = elm;
+    	response = IQ.createResultIQ(reqIQ);
+    	request  = reqIQ;
+    	actor    = actorJID;
+        node     = element.attributeValue("node");
+        
+        try {
+			if ((false == checkNode())
+			    || (false == userHasPermissionToAuthorize())
+					) {
+				outQueue.put(response);
+				return;
+			}
+        } catch (DataStoreException e) {
+        	LOGGER.debug(e);
+			setErrorCondition(
+			    PacketError.Type.wait,
+			    PacketError.Condition.internal_server_error
+			);
+			outQueue.put(response);
+			return;
+        }
+	}
+
+	private boolean userHasPermissionToAuthorize() throws DataStoreException {
+		NodeSubscriptionImpl subscription = dataStore.getUserSubscriptionOfNode(actor.toBareJID(), node);
+		if (null == subscription) {
+			setErrorCondition(PacketError.Type.auth, PacketError.Condition.not_authorized);
+			return false;
+		}
+		if ((false ==subscription.getAffiliation().equals(Affiliations.moderator))
+		    && (false == subscription.getAffiliation().equals(Affiliations.owner))
+		) {
+			setErrorCondition(PacketError.Type.auth,  PacketError.Condition.not_authorized);
+		    return false;
+		}
+		return true;
+	}
+
+	private boolean checkNode() throws DataStoreException {
+		if (null == node) {
+			response.setType(IQ.Type.error);
+	    	Element nodeIdRequired = new DOMElement(
+	            "nodeid-required",
+	            new Namespace("", JabberPubsub.NS_PUBSUB_ERROR)
+	        );
+	    	Element badRequest = new DOMElement(
+	    	    PacketError.Condition.bad_request.toString(),
+	            new Namespace("", JabberPubsub.NS_XMPP_STANZAS)
+	    	);
+	        Element error = new DOMElement("error");
+	        error.addAttribute("type", "modify");
+	        error.add(badRequest);
+	        error.add(nodeIdRequired);
+	        response.setChildElement(error);
+			return false;
+		}
+		if (false == dataStore.nodeExists(node)) {
+			setErrorCondition(
+			    PacketError.Type.cancel,
+			    PacketError.Condition.item_not_found
+			);
+			return false;
+		}
+		return true;
 	}
 
 	/**
--- src/test/java/org/buddycloud/channelserver/packetprocessor/iq/namespace/pubsub/set/SubscriptionEventTest.java
+++ src/test/java/org/buddycloud/channelserver/packetprocessor/iq/namespace/pubsub/set/SubscriptionEventTest.java
@@ -12,6 +12,7 @@ import org.buddycloud.channelserver.channel.node.configuration.NodeConfiguration
 import org.buddycloud.channelserver.channel.node.configuration.field.ChannelTitle;
 import org.buddycloud.channelserver.db.DataStore;
 import org.buddycloud.channelserver.db.DataStoreException;
+import org.buddycloud.channelserver.db.jedis.NodeSubscriptionImpl;
 import org.buddycloud.channelserver.db.mock.Mock;
 import org.buddycloud.channelserver.packetHandler.iq.IQTestHandler;
 import org.dom4j.Element;
@@ -31,6 +32,7 @@ public class SubscriptionEventTest extends IQTestHandler
 	private SubscriptionEvent event;
 	private JID               jid;
 	private Element           element;
+	private String            node;
 	private BlockingQueue<Packet> queue = new LinkedBlockingQueue<Packet>();
 
 	@Before
@@ -41,11 +43,12 @@ public class SubscriptionEventTest extends IQTestHandler
 		event      = new SubscriptionEvent(queue, dataStore);
 		jid        = new JID("juliet@shakespeare.lit");
 		request    = readStanzaAsIq("/iq/pubsub/channel/create/request.stanza");
+		node       = "/user/capulet@shakespeare.lit/posts";
 		
 		event.setServerDomain("shakespeare.lit");
 		
-		element = new BaseElement("create");
-		element.addAttribute("node", "/user/capulet@shakespeare.lit/posts");
+		element = new BaseElement("subscription");
+		element.addAttribute("node", node);
 	}
 
 	@Test
@@ -61,4 +64,100 @@ public class SubscriptionEventTest extends IQTestHandler
 		Element element = new BaseElement("not-create");
 		assertFalse(event.accept(element));
 	}
+	
+	@Test
+	public void testNotProvidingNodeAttributeReturnsErrorStanza() throws Exception
+	{
+	    BaseElement element = new BaseElement("subscription");
+        event.process(element, jid, request, null);
+        Packet response = queue.poll(100, TimeUnit.MILLISECONDS);
+        
+	    PacketError error = response.getError();
+		assertNotNull(error);
+		assertEquals(PacketError.Type.modify, error.getType());
+		assertEquals("nodeid-required", error.getApplicationConditionName());
+	}
+	
+	@Test
+	public void testDataStoreExceptionResultsInInternalServerErrorStanza() throws Exception
+	{
+    	DataStore dataStoreMock = Mockito.mock(Mock.class);
+		Mockito
+		    .when(dataStoreMock.nodeExists(node))
+		    .thenThrow(DataStoreException.class);
+        event.setDataStore(dataStoreMock);
+        
+        event.process(element, jid, request, null);
+        Packet response = queue.poll(100, TimeUnit.MILLISECONDS);
+        
+	    PacketError error = response.getError();
+		assertNotNull(error);
+		assertEquals(PacketError.Type.wait, error.getType());
+		assertEquals(PacketError.Condition.internal_server_error, error.getCondition());
+	}
+	
+    @Test
+    public void testNonExistantNodeRetunsErrorStanza() throws Exception
+    {
+    	DataStore dataStoreMock = Mockito.mock(Mock.class);
+		Mockito
+		    .when(dataStoreMock.nodeExists(node))
+		    .thenReturn(false);
+        event.setDataStore(dataStoreMock);
+        
+        event.process(element, jid, request, null);
+        Packet response = queue.poll(100, TimeUnit.MILLISECONDS);
+        
+	    PacketError error = response.getError();
+		assertNotNull(error);
+		assertEquals(PacketError.Type.cancel, error.getType());
+		assertEquals(PacketError.Condition.item_not_found, error.getCondition());
+    }
+
+    @Test
+    public void testUserWithoutAffiliationReturnsErrorStanza() throws Exception
+    {
+    	DataStore dataStoreMock = Mockito.mock(Mock.class);
+		Mockito
+		    .when(dataStoreMock.nodeExists(node))
+		    .thenReturn(true);
+		Mockito
+		    .when(dataStoreMock.getUserSubscriptionOfNode(jid.toBareJID(), node))
+		    .thenReturn(null);
+        event.setDataStore(dataStoreMock);
+        
+        event.process(element, jid, request, null);
+        Packet response = queue.poll(100, TimeUnit.MILLISECONDS);
+        
+	    PacketError error = response.getError();
+		assertNotNull(error);
+		assertEquals(PacketError.Type.auth, error.getType());
+		assertEquals(PacketError.Condition.not_authorized, error.getCondition());
+    }
+    
+    @Test
+    public void testUserWhoIsntOwnerOrModeratorCantUpdateSubscription() throws Exception
+    {
+    	NodeSubscriptionImpl subscriptionMock = Mockito.mock(NodeSubscriptionImpl.class);
+    	Mockito
+    	    .when(subscriptionMock.getAffiliation())
+    	    .thenReturn("subscribed");
+    	
+    	DataStore dataStoreMock = Mockito.mock(Mock.class);
+		Mockito
+		    .when(dataStoreMock.nodeExists(node))
+		    .thenReturn(true);
+		Mockito
+		    .when(dataStoreMock.getUserSubscriptionOfNode(jid.toBareJID(), node))
+		    .thenReturn(subscriptionMock);
+        event.setDataStore(dataStoreMock);
+        
+        event.process(element, jid, request, null);
+        Packet response = queue.poll(100, TimeUnit.MILLISECONDS);
+        
+	    PacketError error = response.getError();
+		assertNotNull(error);
+		assertEquals(PacketError.Type.auth, error.getType());
+		assertEquals(PacketError.Condition.not_authorized, error.getCondition());
+    }
 }

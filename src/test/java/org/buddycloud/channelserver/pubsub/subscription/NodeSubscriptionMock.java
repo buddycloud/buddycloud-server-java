e4f1a3f2cf14ef4bf33892ced10a73ae5501b9c8
Lloyd Watkin <lloyd.watkin@surevine.com>
2012-09-11 17:52:12 +0100

    Adding more items tests

--- src/test/java/org/buddycloud/channelserver/pubsub/subscription/NodeSubscriptionMock.java
+++ src/test/java/org/buddycloud/channelserver/pubsub/subscription/NodeSubscriptionMock.java
@@ -1,46 +1,56 @@
 package org.buddycloud.channelserver.pubsub.subscription;
 
-public class NodeSubscriptionMock 
-    implements NodeSubscription
-{
+public class NodeSubscriptionMock implements NodeSubscription {
 	private String subscriber;
-	
-	public NodeSubscriptionMock(String jid)
-	{
+	private String affiliation;
+	private String subscription;
+	private String foreignChannelServer;
+
+	public void setBareJID(String jid) {
+		this.subscriber = jid;
+	}
+
+	public void setAffiliation(String affiliation) {
+		this.affiliation = affiliation;
+	}
+
+	public void setSubscription(String subscription) {
+		this.subscription = subscription;
+	}
+
+	public void setForeignChannelServer(String foreignChannelServer) {
+		this.foreignChannelServer = foreignChannelServer;
+	}
+
+	public NodeSubscriptionMock(String jid) {
 		subscriber = jid;
 	}
 
 	@Override
-	public String getAffiliation()
-	{
+	public String getAffiliation() {
 		// TODO Auto-generated method stub
-		return null;
+		return affiliation;
 	}
 
 	@Override
-	public String getSubscription()
-	{
+	public String getSubscription() {
 		// TODO Auto-generated method stub
-		return null;
+		return subscription;
 	}
 
 	@Override
-	public String getBareJID()
-	{
+	public String getBareJID() {
 		return subscriber;
 	}
-	
+
 	@Override
-	public String getNode()
-	{
+	public String getNode() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public String getForeignChannelServer()
-	{
-		// TODO Auto-generated method stub
-		return null;
+	public String getForeignChannelServer() {
+		return foreignChannelServer;
 	}
 }

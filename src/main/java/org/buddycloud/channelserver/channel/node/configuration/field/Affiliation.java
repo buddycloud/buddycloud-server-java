c5ed0188d4690e4cadd6c11e6231166f48804046
Lloyd Watkin <lloyd.watkin@surevine.com>
2012-09-24 14:47:11 +0100

    Fix for default_affiliation valid values

--- src/main/java/org/buddycloud/channelserver/channel/node/configuration/field/Affiliation.java
+++ src/main/java/org/buddycloud/channelserver/channel/node/configuration/field/Affiliation.java
@@ -1,30 +1,34 @@
 package org.buddycloud.channelserver.channel.node.configuration.field;
 
-public class Affiliation extends Field
-{
-	public static final String FIELD_NAME    = "pubsub#default_affiliation";
-	public static final String DEFAULT_VALUE = Affiliation.models.MEMBER.toString();
-	
-	public enum models { 
-		FOLLOWER_AND_POST("publisher"), MEMBER("follower");
+public class Affiliation extends Field {
+	public static final String FIELD_NAME = "pubsub#default_affiliation";
+	public static final String DEFAULT_VALUE = Affiliation.models.MEMBER
+			.toString();
+
+	public enum models {
+		FOLLOWER_AND_POST("publisher"), MEMBER("follower"), OWNER("owner"), MODERATOR(
+				"moderator");
 		String model = null;
+
 		private models(String model) {
 			this.model = model;
 		}
+
 		public String toString() {
-	        return model;
-	    }
+			return model;
+		}
 	}
 
-	public Affiliation()
-	{
+	public Affiliation() {
 		name = FIELD_NAME;
 	}
-	
-	public boolean isValid()
-	{
-		return (getValue().equals(Affiliation.models.FOLLOWER_AND_POST.toString())
-			|| getValue().equals(Affiliation.models.MEMBER.toString())
-		);
+
+	public boolean isValid() {
+		return (getValue().equals(
+				Affiliation.models.FOLLOWER_AND_POST.toString()) 
+		    || getValue().equals(Affiliation.models.MEMBER.toString())
+		    || getValue().equals(Affiliation.models.OWNER.toString())
+		    || getValue().equals(Affiliation.models.MODERATOR.toString())
+	    );
 	}
 }

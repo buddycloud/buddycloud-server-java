275c49666a9cb06414745187ce0a8edb31bf9cb3
Lloyd Watkin <lloyd.watkin@surevine.com>
2013-01-04 12:44:58 +0000

    Support meta data in post entry.
    Already covered by node server as it more loosely covers entry data

--- src/main/java/org/buddycloud/channelserver/channel/ValidateEntry.java
+++ src/main/java/org/buddycloud/channelserver/channel/ValidateEntry.java
@@ -19,6 +19,7 @@ public class ValidateEntry {
    
 	private String errorMsg = "";
 	private String inReplyTo;
+	private Element meta;
 	
 	Map<String, String>params = new HashMap<String, String>();
 	
@@ -97,6 +98,10 @@ public class ValidateEntry {
 				inReplyTo       = tokens[2];
 			}
 		}
+		Element meta = this.entry.element("meta");
+		if (null != meta) {
+			this.meta = meta;
+		}
 		return true;
 	}
 	
@@ -122,6 +127,9 @@ public class ValidateEntry {
              <activity:object>
                 <activity:object-type>note</activity:object-type>
              </activity:object>
+             <meta>
+                ... additional meta ...
+             </meta>
           </entry>
 		 */
 		String id       = UUID.randomUUID().toString();
@@ -178,6 +186,9 @@ public class ValidateEntry {
 		activity_object.addElement("activity:object-type")
 		               .setText(postType);
 		
+		if (null != meta) {
+			entry.add(meta.createCopy());
+		}
 		return entry;
 	}
 	

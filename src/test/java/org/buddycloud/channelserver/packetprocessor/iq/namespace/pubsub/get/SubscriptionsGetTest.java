d6881d79d7052c440020c621ea5ccc906db74cb9
Lloyd Watkin <lloyd.watkin@surevine.com>
2013-04-05 13:32:55 +0100

    Add *very* basic tests for SubscriptionsGet

---
+++ src/test/java/org/buddycloud/channelserver/packetprocessor/iq/namespace/pubsub/get/SubscriptionsGetTest.java
@@ -0,0 +1,74 @@
+package org.buddycloud.channelserver.packetprocessor.iq.namespace.pubsub.get;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+import junit.framework.Assert;
+
+import org.buddycloud.channelserver.channel.ChannelManager;
+import org.buddycloud.channelserver.db.ClosableIteratorImpl;
+import org.buddycloud.channelserver.db.CloseableIterator;
+import org.buddycloud.channelserver.db.exception.NodeStoreException;
+import org.buddycloud.channelserver.db.mock.Mock;
+import org.buddycloud.channelserver.packetHandler.iq.IQTestHandler;
+import org.buddycloud.channelserver.packetprocessor.iq.namespace.pubsub.JabberPubsub;
+import org.buddycloud.channelserver.pubsub.accessmodel.AccessModels;
+import org.buddycloud.channelserver.pubsub.affiliation.Affiliations;
+import org.buddycloud.channelserver.pubsub.model.NodeItem;
+import org.buddycloud.channelserver.pubsub.model.NodeSubscription;
+import org.buddycloud.channelserver.pubsub.model.impl.NodeAffiliationImpl;
+import org.buddycloud.channelserver.pubsub.model.impl.NodeSubscriptionImpl;
+import org.buddycloud.channelserver.pubsub.subscription.Subscriptions;
+import org.buddycloud.channelserver.utils.node.NodeAclRefuseReason;
+import org.buddycloud.channelserver.utils.node.NodeViewAcl;
+import org.dom4j.Element;
+import org.dom4j.Namespace;
+import org.dom4j.QName;
+import org.dom4j.tree.BaseElement;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.xmpp.packet.IQ;
+import org.xmpp.packet.JID;
+import org.xmpp.packet.Packet;
+import org.xmpp.packet.PacketError;
+import org.xmpp.resultsetmanagement.ResultSetImpl;
+
+public class SubscriptionsGetTest extends IQTestHandler {
+
+	private IQ request;
+	private SubscriptionsGet subscriptionsGet;
+	private Element element;
+	private BlockingQueue<Packet> queue = new LinkedBlockingQueue<Packet>();
+
+	private String node = "/user/pamela@denmark.lit/posts";
+	private JID jid = new JID("juliet@shakespeare.lit");
+	private ChannelManager channelManager;
+
+	@Before
+	public void setUp() throws Exception {
+
+		queue = new LinkedBlockingQueue<Packet>();
+		subscriptionsGet = new SubscriptionsGet(queue, channelManager);
+		element = new BaseElement("subscriptions");
+
+		channelManager = Mockito.mock(Mock.class);
+		Mockito.when(channelManager.isLocalNode(Mockito.anyString()))
+				.thenReturn(true);
+		subscriptionsGet.setChannelManager(channelManager);
+	}
+
+	@Test
+	public void testPassingAffiliationsAsElementNameReturnsTrue() {
+		Assert.assertTrue(subscriptionsGet.accept(element));
+	}
+
+	@Test
+	public void testPassingNotCreateAsElementNameReturnsFalse() {
+		Element element = new BaseElement("not-subscriptions");
+		Assert.assertFalse(subscriptionsGet.accept(element));
+	}
+}

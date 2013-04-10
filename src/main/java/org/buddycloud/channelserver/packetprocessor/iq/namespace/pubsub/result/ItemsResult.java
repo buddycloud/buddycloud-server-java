1732a8398388cb0c8986982fa488dac11a8bc505
Lloyd Watkin <lloyd.watkin@surevine.com>
2013-04-10 18:27:59 +0100

    Tests and code complete for item results. Need to check 'nodes' keys in DB before sign-off.

--- src/main/java/org/buddycloud/channelserver/packetprocessor/iq/namespace/pubsub/result/ItemsResult.java
+++ src/main/java/org/buddycloud/channelserver/packetprocessor/iq/namespace/pubsub/result/ItemsResult.java
@@ -1,7 +1,18 @@
 package org.buddycloud.channelserver.packetprocessor.iq.namespace.pubsub.result;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import org.apache.commons.lang.time.DateFormatUtils;
+import org.apache.log4j.Logger;
 import org.buddycloud.channelserver.channel.ChannelManager;
+import org.buddycloud.channelserver.channel.ValidateEntry;
+import org.buddycloud.channelserver.db.exception.NodeStoreException;
 import org.buddycloud.channelserver.packetprocessor.iq.namespace.pubsub.PubSubElementProcessorAbstract;
+import org.buddycloud.channelserver.packetprocessor.iq.namespace.pubsub.set.SubscribeSet;
+import org.buddycloud.channelserver.pubsub.model.impl.NodeItemImpl;
 import org.dom4j.Element;
 import org.xmpp.packet.IQ;
 import org.xmpp.packet.JID;
@@ -9,8 +20,11 @@ import org.xmpp.packet.JID;
 public class ItemsResult extends PubSubElementProcessorAbstract {
 
 	private static final String MISSING_NODE = "Missing node";
-	private static final String MISSING_ITEMS_ELEMENT = "Missing items element";
+    private static final String DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss'Z'";
+ 
+	private static final Logger logger = Logger.getLogger(ItemsResult.class);
 	private String node;
+	private SimpleDateFormat sdf = new SimpleDateFormat(DATE_FORMAT);
 	
 	public ItemsResult(ChannelManager channelManager) {
 		this.channelManager = channelManager;
@@ -18,13 +32,37 @@ public class ItemsResult extends PubSubElementProcessorAbstract {
 
 	public void process(Element elm, JID actorJID, IQ reqIQ, Element rsm)
 			throws Exception {
-        node = elm.attributeValue("node");
-        if ((null == node) || (true == node.equals(""))) {
-        	throw new NullPointerException(MISSING_NODE);
-        }
-        Element items = elm.element("items");
-		if (items == null) {
-			throw new NullPointerException(MISSING_ITEMS_ELEMENT);
+
+		this.request = reqIQ;
+
+		node = elm.attributeValue("node");
+
+		if ((null == node) || (true == node.equals(""))) {
+			throw new NullPointerException(MISSING_NODE);
+		}
+
+		List<Element> items = request.getElement()
+				.element("pubsub")
+				.element("items")
+				.elements("item");
+
+		for (Element item : items) {
+			processItem(item);
+		}
+	}
+
+	private void processItem(Element item) throws ParseException, NodeStoreException {
+
+		Element entry = item.element("entry");
+		
+		try {
+		    Date updatedDate = sdf.parse(entry.elementText("updated"));
+			NodeItemImpl nodeItem = new NodeItemImpl(node, entry.elementText("id"),
+					updatedDate, entry.asXML());
+			channelManager.addNodeItem(nodeItem);
+		} catch (ParseException e) {
+			logger.error(e);
+			return;
 		}
 	}
 
--- src/test/java/org/buddycloud/channelserver/packetprocessor/iq/namespace/pubsub/result/ItemsResultTest.java
+++ src/test/java/org/buddycloud/channelserver/packetprocessor/iq/namespace/pubsub/result/ItemsResultTest.java
@@ -3,6 +3,7 @@ package org.buddycloud.channelserver.packetprocessor.iq.namespace.pubsub.result;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Map;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.TimeUnit;
@@ -15,6 +16,7 @@ import org.buddycloud.channelserver.db.CloseableIterator;
 import org.buddycloud.channelserver.db.exception.NodeStoreException;
 import org.buddycloud.channelserver.db.mock.Mock;
 import org.buddycloud.channelserver.packetHandler.iq.IQTestHandler;
+import org.buddycloud.channelserver.packetprocessor.iq.IQProcessor;
 import org.buddycloud.channelserver.packetprocessor.iq.namespace.pubsub.JabberPubsub;
 import org.buddycloud.channelserver.pubsub.accessmodel.AccessModels;
 import org.buddycloud.channelserver.pubsub.affiliation.Affiliations;
@@ -56,7 +58,9 @@ public class ItemsResultTest extends IQTestHandler {
 		queue = new LinkedBlockingQueue<Packet>();
 		itemsResult = new ItemsResult(channelManager);
 		request = readStanzaAsIq("/iq/pubsub/items/reply.stanza");
+
 		element = new BaseElement("items");
+		element.addAttribute("node", node);
 
 		channelManager = Mockito.mock(Mock.class);
 		itemsResult.setChannelManager(channelManager);
@@ -73,18 +77,83 @@ public class ItemsResultTest extends IQTestHandler {
 		Assert.assertFalse(itemsResult.accept(element));
 	}
 
-	@Test(expected=NullPointerException.class)
+	@Test(expected = NullPointerException.class)
 	public void testMissingNodeAttributeThrowsException() throws Exception {
+		Element element = new BaseElement("items");
+		itemsResult.process(element, jid, request, null);
+	}
+
+	@Test
+	public void testNoItemsResultsInNoAdditionsToDatabase() throws Exception {
+
+		Mockito.verify(channelManager, Mockito.never()).addNodeItem(
+				Mockito.any(NodeItem.class));
+
+		request = toIq("<iq type=\"result\" id=\"items1\" "
+				+ "from=\"channels.shakespeare.lit\" "
+				+ "to=\"francisco@denmark.lit/barracks\">"
+				+ "<pubsub xmlns=\"http://jabber.org/protocol/pubsub\">"
+				+ "<items node=\"/user/francisco@denmark.lit/posts\">"
+				+ "</items></pubsub></iq>");
+
+		itemsResult.process(element, jid, request, null);
+	}
+
+	@Test
+	public void testNodeCreatedInDatabaseIfItDoesntExist() throws Exception {
+		Mockito.when(channelManager.nodeExists(Mockito.anyString()))
+				.thenReturn(false);
+		Mockito.verify(channelManager, Mockito.times(1)).createNode(
+				Mockito.any(JID.class), Mockito.anyString(), Mockito.anyMap());
+
+		request = toIq("<iq type=\"result\" id=\"items1\" "
+				+ "from=\"channels.shakespeare.lit\" "
+				+ "to=\"francisco@denmark.lit/barracks\">"
+				+ "<pubsub xmlns=\"http://jabber.org/protocol/pubsub\">"
+				+ "<items node=\"/user/francisco@denmark.lit/posts\">"
+				+ "</items></pubsub></iq>");
+
+		itemsResult.process(element, jid, request, null);
+	}
+
+	@Test
+	public void testNodeNotCreatedInDatabaseIfAlreadyExists() throws Exception {
+		Mockito.when(channelManager.nodeExists(Mockito.anyString()))
+				.thenReturn(true);
+		Mockito.verify(channelManager, Mockito.never()).createNode(
+				Mockito.any(JID.class), Mockito.anyString(), Mockito.anyMap());
+
+		request = toIq("<iq type=\"result\" id=\"items1\" "
+				+ "from=\"channels.shakespeare.lit\" "
+				+ "to=\"francisco@denmark.lit/barracks\">"
+				+ "<pubsub xmlns=\"http://jabber.org/protocol/pubsub\">"
+				+ "<items node=\"/user/francisco@denmark.lit/posts\">"
+				+ "</items></pubsub></iq>");
+
+		itemsResult.process(element, jid, request, null);
+	}
+
+	@Test
+	public void testItemWithInvalidDateIsNotAddedToDatabase() throws Exception {
+		
+		Mockito.when(channelManager.nodeExists(Mockito.anyString()))
+		.thenReturn(true);
+		
+		request = toIq(readStanzaAsString("/iq/pubsub/items/reply.stanza")
+				.replaceFirst("2011-11-27T19:05:58Z", "star date 23252455"));
+		Mockito.verify(channelManager, Mockito.atMost(3)).addNodeItem(
+				Mockito.any(NodeItem.class));
 		itemsResult.process(element, jid, request, null);
 	}
 	
-	@Test(expected=NullPointerException.class)
-	public void testMissingItemsElementThrowsException() throws Exception {
+	@Test
+	public void testSendingValidItemsResultsInCorrectNumberOfDatabaseEntries() throws Exception {
 		
-		request = toIq(readStanzaAsString(
-				"/iq/pubsub/item/delete/request.stanza").replaceFirst(
-				"<item id='item-id' notify='true' />", ""));
+		Mockito.when(channelManager.nodeExists(Mockito.anyString()))
+		.thenReturn(true);
 		
+		Mockito.verify(channelManager, Mockito.atMost(4)).addNodeItem(
+				Mockito.any(NodeItem.class));
 		itemsResult.process(element, jid, request, null);
 	}
 }

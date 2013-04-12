c44bc5bc66eff2baa1fa16d146a747d5d008f686
Lloyd Watkin <lloyd.watkin@surevine.com>
2013-04-12 17:23:01 +0100

    Affiliation result stanza support

--- src/main/java/org/buddycloud/channelserver/packetprocessor/iq/namespace/pubsub/result/AffiliationsResult.java
+++ src/main/java/org/buddycloud/channelserver/packetprocessor/iq/namespace/pubsub/result/AffiliationsResult.java
@@ -1,28 +1,71 @@
 package org.buddycloud.channelserver.packetprocessor.iq.namespace.pubsub.result;
 
+import java.util.List;
+
+import org.apache.log4j.Logger;
 import org.buddycloud.channelserver.channel.ChannelManager;
+import org.buddycloud.channelserver.db.exception.NodeStoreException;
 import org.buddycloud.channelserver.packetprocessor.iq.namespace.pubsub.PubSubElementProcessorAbstract;
+import org.buddycloud.channelserver.pubsub.affiliation.Affiliations;
 import org.dom4j.Element;
 import org.xmpp.packet.IQ;
 import org.xmpp.packet.JID;
 
 public class AffiliationsResult extends PubSubElementProcessorAbstract {
 
+	private String node;
+	private IQ request;
+	private boolean ownerRequest;
+	private String lastNode = "";
+
+	private static final Logger logger = Logger
+			.getLogger(AffiliationsResult.class);
+
 	public AffiliationsResult(ChannelManager channelManager) {
-		// TODO Auto-generated constructor stub
+		this.channelManager = channelManager;
 	}
 
 	@Override
 	public void process(Element elm, JID actorJID, IQ reqIQ, Element rsm)
 			throws Exception {
-		// TODO Auto-generated method stub
+		this.request = reqIQ;
 
+		if (-1 != request.getFrom().toString().indexOf("@")) {
+			logger.debug("Ignoring result packet, only interested in stanzas "
+					+ "from other buddycloud servers");
+			return;
+		}
+
+		node         = elm.attributeValue("node");
+		ownerRequest = ((null == node) || (true == node.equals("")));
+
+		@SuppressWarnings("unchecked")
+		List<Element> affiliations = request.getElement().element("pubsub")
+				.element("affiliations").elements("affiliation");
+
+		for (Element affiliation : affiliations) {
+			addAffiliation(affiliation);
+		}
+	}
+
+	private void addAffiliation(Element affiliation) throws NodeStoreException {
+
+		if (true == ownerRequest) {
+			node = affiliation.attributeValue("node");
+		}
+
+		if ((false == lastNode.equals(node))
+				&& (false == channelManager.nodeExists(node)))
+			channelManager.addRemoteNode(node);
+
+		JID jid = new JID(affiliation.attributeValue("jid"));
+		channelManager.setUserAffiliation(node, jid, Affiliations
+				.createFromString(affiliation.attributeValue("affiliation")));
+		lastNode = node;
 	}
 
 	@Override
 	public boolean accept(Element elm) {
-		// TODO Auto-generated method stub
-		return false;
+		return elm.getName().equals("affiliations");
 	}
-
 }
---
+++ src/test/java/org/buddycloud/channelserver/packetprocessor/iq/namespace/pubsub/result/AffiliationsResultTest.java
@@ -0,0 +1,136 @@
+package org.buddycloud.channelserver.packetprocessor.iq.namespace.pubsub.result;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+import junit.framework.Assert;
+
+import org.apache.commons.lang.StringUtils;
+import org.buddycloud.channelserver.channel.ChannelManager;
+import org.buddycloud.channelserver.db.ClosableIteratorImpl;
+import org.buddycloud.channelserver.db.CloseableIterator;
+import org.buddycloud.channelserver.db.exception.NodeStoreException;
+import org.buddycloud.channelserver.db.mock.Mock;
+import org.buddycloud.channelserver.packetHandler.iq.IQTestHandler;
+import org.buddycloud.channelserver.packetprocessor.iq.IQProcessor;
+import org.buddycloud.channelserver.packetprocessor.iq.namespace.pubsub.JabberPubsub;
+import org.buddycloud.channelserver.pubsub.accessmodel.AccessModels;
+import org.buddycloud.channelserver.pubsub.affiliation.Affiliation;
+import org.buddycloud.channelserver.pubsub.affiliation.Affiliations;
+import org.buddycloud.channelserver.pubsub.model.NodeAffiliation;
+import org.buddycloud.channelserver.pubsub.model.NodeItem;
+import org.buddycloud.channelserver.pubsub.model.NodeSubscription;
+import org.buddycloud.channelserver.pubsub.model.impl.NodeAffiliationImpl;
+import org.buddycloud.channelserver.pubsub.model.impl.NodeSubscriptionImpl;
+import org.buddycloud.channelserver.pubsub.subscription.Subscriptions;
+import org.buddycloud.channelserver.utils.node.NodeAclRefuseReason;
+import org.buddycloud.channelserver.utils.node.NodeViewAcl;
+import org.dom4j.DocumentException;
+import org.dom4j.Element;
+import org.dom4j.Namespace;
+import org.dom4j.QName;
+import org.dom4j.tree.BaseElement;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.xmpp.packet.IQ;
+import org.xmpp.packet.JID;
+import org.xmpp.packet.Packet;
+import org.xmpp.packet.PacketError;
+import org.xmpp.resultsetmanagement.ResultSetImpl;
+
+public class AffiliationsResultTest extends IQTestHandler {
+
+	private IQ resultWithNode;
+	private IQ resultNoNode;
+	private AffiliationsResult affiliationsResult;
+	private Element element;
+
+	private String node = "/user/pamela@denmark.lit/posts";
+	private JID jid = new JID("juliet@shakespeare.lit");
+	private ChannelManager channelManager;
+
+	@Before
+	public void setUp() throws Exception {
+
+		channelManager = Mockito.mock(Mock.class);
+
+		affiliationsResult = new AffiliationsResult(channelManager);
+		resultWithNode = readStanzaAsIq("/iq/pubsub/affiliations/reply-with-node.stanza");
+		resultNoNode = readStanzaAsIq("/iq/pubsub/affiliations/reply-no-node.stanza");
+
+		element = new BaseElement("affiliations");
+		element.addAttribute("node", node);
+
+		affiliationsResult.setChannelManager(channelManager);
+	}
+
+	@Test
+	public void testPassingAffiliationsAsElementNameReturnsTrue() {
+		Assert.assertTrue(affiliationsResult.accept(element));
+	}
+
+	@Test
+	public void testPassingNotAffiliationsAsElementNameReturnsFalse() {
+		Element element = new BaseElement("not-affiliations");
+		Assert.assertFalse(affiliationsResult.accept(element));
+	}
+
+	@Test(expected = NullPointerException.class)
+	public void testInvalidStanzaThrowsException() throws Exception {
+
+		IQ result = toIq("<iq type=\"result\" id=\"affiliations1\" "
+				+ "from=\"channels.shakespeare.lit\" "
+				+ "to=\"channels.denmark.lit\">"
+				+ "<pubsub xmlns=\"http://jabber.org/protocol/pubsub#owner\" />"
+				+ "</iq>");
+
+		affiliationsResult.process(element, jid, result, null);
+	}
+
+	@Test
+	public void testNoAffiliationsCausesNoDatastoreInsert() throws Exception {
+		IQ result = toIq("<iq type=\"result\" id=\"affiliations1\" "
+				+ "from=\"channels.shakespeare.lit\" "
+				+ "to=\"channels.denmark.lit\">"
+				+ "<pubsub xmlns=\"http://jabber.org/protocol/pubsub#owner\">"
+				+ "<affiliations />"
+				+ "</pubsub>"
+				+ "</iq>");
+
+		affiliationsResult.process(element, jid, result, null);
+		
+		Mockito.verify(channelManager, Mockito.times(0)).setUserAffiliation(
+				Mockito.anyString(), Mockito.any(JID.class),
+				Mockito.any(Affiliations.class));
+	}
+
+	@Test
+	public void testOwnerAffiliationsResultStanzaHandledCorrectly()
+			throws Exception {
+		element = new BaseElement("affiliations");
+		affiliationsResult.process(element, jid, resultNoNode, null);
+
+		Mockito.verify(channelManager, Mockito.times(1)).setUserAffiliation(
+				Mockito.eq("/user/pamela@denmark.lit/posts"),
+				Mockito.any(JID.class), Mockito.any(Affiliations.class));
+		Mockito.verify(channelManager, Mockito.times(1)).setUserAffiliation(
+				Mockito.eq("/user/francisco@denmark.lit/posts"),
+				Mockito.any(JID.class), Mockito.any(Affiliations.class));
+	}
+
+	@Test
+	public void testNodeAffiliationsResultStanzaHandledCorrectly()
+			throws Exception {
+		affiliationsResult.process(element, jid, resultWithNode, null);
+
+		Mockito.verify(channelManager, Mockito.times(2)).setUserAffiliation(
+				Mockito.eq("/user/pamela@denmark.lit/posts"),
+				Mockito.any(JID.class), Mockito.any(Affiliations.class));
+	}
+}

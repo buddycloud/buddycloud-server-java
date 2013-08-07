8b2f5fe7204e5dafdbaf377027327d0be9fa0d2c
abmargb <abmargb@gmail.com>
2013-08-07 17:32:26 +0200

    Better logging. Refatoring for unused fields.

--- src/main/java/org/buddycloud/channelserver/Main.java
+++ src/main/java/org/buddycloud/channelserver/Main.java
@@ -1,21 +1,22 @@
 package org.buddycloud.channelserver;
 
-import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 
-public class Main
-{
-    private static Logger LOGGER = Logger.getLogger(Main.class);
+public class Main {
+    
+	private static Logger LOGGER = Logger.getLogger(Main.class);
 	
     public static void main(String[] args) {
-        
-        PropertyConfigurator.configure("log4j.properties");
-        Logger.getLogger(Main.class).setLevel(Level.DEBUG);
-        
-        TopicsComponent topicsComponent;
-        XmppComponent xmppComponent;
-        
+        try {
+        	startComponents();
+		} catch (Exception e) {
+			LOGGER.error("Failed during initialization.", e);
+		}
+    }
+
+	private static void startComponents() throws Exception {
+		PropertyConfigurator.configure("log4j.properties");
         LOGGER.info("Starting Buddycloud channel mockup version...");
 
     	Configuration conf = Configuration.getInstance(); 
@@ -25,19 +26,23 @@ public class Main
             + "' and trying to claim address '" 
             + conf.getProperty("xmpp.subdomain") + "'.");
 
-        try {
-            xmppComponent = new XmppComponent(conf, conf.getProperty("server.domain.channels"));
-            xmppComponent.run();
-            topicsComponent = new TopicsComponent(conf, conf.getProperty("server.domain.topics"));
-            topicsComponent.run();
-        } catch (Exception e1) {
-            LOGGER.error("Failed during initialization.", e1);
+        String channelDomain = conf.getProperty("server.domain.channels");
+        String topicDomain = conf.getProperty("server.domain.topics");
+        
+        if (channelDomain == null) {
+        	throw new IllegalArgumentException("Property server.domain.channels is mandatory.");
         }
-        run();
-    }
+        
+        if (topicDomain == null) {
+        	throw new IllegalArgumentException("Property server.domain.topics is mandatory.");
+        }
+        
+		new XmppComponent(conf, channelDomain).run();
+		new TopicsComponent(conf, topicDomain).run();
+        hang();
+	}
 
-	private static void run()
-	{
+	private static void hang() {
 		while (true) {
 		    try {
 		        Thread.sleep(5000);
--- src/main/java/org/buddycloud/channelserver/TopicsComponent.java
+++ src/main/java/org/buddycloud/channelserver/TopicsComponent.java
@@ -2,38 +2,24 @@ package org.buddycloud.channelserver;
 
 import java.util.Properties;
 
-import org.apache.log4j.Logger;
 import org.jivesoftware.whack.ExternalComponentManager;
 import org.xmpp.component.ComponentException;
-import org.logicalcobwebs.proxool.ProxoolException;
-import org.logicalcobwebs.proxool.configuration.PropertyConfigurator;
 
 public class TopicsComponent {
 
-	private static final Logger logger = Logger.getLogger(TopicsComponent.class);
 	private String hostname;
 	private int socket;
 	
 	private String domain;
 	private String password;
-	private Properties configuration;
 	private TopicsEngine topicsEngine;
 	
 	public TopicsComponent(Properties configuration, String domain) {
-		if (null == domain) {
-			return;
-		}
-	    setConf(configuration);
 		hostname = configuration.getProperty("xmpp.host");
 		socket = Integer.valueOf(configuration.getProperty("xmpp.port"));
 		this.domain = domain;
 		password = configuration.getProperty("xmpp.secretkey");
 		topicsEngine = new TopicsEngine(configuration);
-
-	}
-	
-	public void setConf(Properties configuration) {
-		this.configuration = configuration;
 	}
 	
 	public void run() throws ComponentException {

f7d5d480b2b48738cef933c3f57a20ed810b9193
Lloyd Watkin <lloyd.watkin@surevine.com>
2013-09-13 15:30:42 +0100

    Add template test files

---
+++ src/test/java/org/buddycloud/channelserver/packetprocessor/iq/namespace/search/SearchSetTest.java
@@ -0,0 +1,69 @@
+package org.buddycloud.channelserver.packetprocessor.iq.namespace.search;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+import junit.framework.Assert;
+
+import org.buddycloud.channelserver.channel.ChannelManager;
+import org.buddycloud.channelserver.db.ClosableIteratorImpl;
+import org.buddycloud.channelserver.db.CloseableIterator;
+import org.buddycloud.channelserver.db.exception.NodeStoreException;
+import org.buddycloud.channelserver.packetHandler.iq.IQTestHandler;
+import org.buddycloud.channelserver.packetprocessor.iq.namespace.pubsub.JabberPubsub;
+import org.buddycloud.channelserver.pubsub.accessmodel.AccessModels;
+import org.buddycloud.channelserver.pubsub.affiliation.Affiliations;
+import org.buddycloud.channelserver.pubsub.model.NodeItem;
+import org.buddycloud.channelserver.pubsub.model.NodeSubscription;
+import org.buddycloud.channelserver.pubsub.model.impl.NodeAffiliationImpl;
+import org.buddycloud.channelserver.pubsub.model.impl.NodeItemImpl;
+import org.buddycloud.channelserver.pubsub.model.impl.NodeSubscriptionImpl;
+import org.buddycloud.channelserver.pubsub.subscription.Subscriptions;
+import org.buddycloud.channelserver.utils.node.NodeAclRefuseReason;
+import org.buddycloud.channelserver.utils.node.NodeViewAcl;
+import org.dom4j.Element;
+import org.dom4j.Namespace;
+import org.dom4j.QName;
+import org.dom4j.tree.BaseElement;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.xmpp.packet.IQ;
+import org.xmpp.packet.JID;
+import org.xmpp.packet.Packet;
+import org.xmpp.packet.PacketError;
+import org.xmpp.resultsetmanagement.ResultSetImpl;
+
+public class SearchSetTest extends IQTestHandler {
+
+	private IQ request;
+	private Element element;
+	private BlockingQueue<Packet> queue = new LinkedBlockingQueue<Packet>();
+
+	private ChannelManager channelManager;
+
+	private SearchSet search;
+
+	@Before
+	public void setUp() throws Exception {
+
+		queue = new LinkedBlockingQueue<Packet>();
+		channelManager = Mockito.mock(ChannelManager.class);
+
+		search = new SearchSet(queue, channelManager);
+
+	}
+
+	@Test
+	public void testTrueIsTrue() {
+		Assert.assertTrue(true);
+	}
+
+}

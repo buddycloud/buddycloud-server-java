42ed12a204f8c71de8cf2801a64eef1f3034d957
Martin Hewitt <martin.hewitt@surevine.com>
2013-09-16 12:20:50 +0100

    Added test for Set validation and assurance

--- src/main/java/org/buddycloud/channelserver/packetprocessor/iq/namespace/search/SearchSet.java
+++ src/main/java/org/buddycloud/channelserver/packetprocessor/iq/namespace/search/SearchSet.java
@@ -1,26 +1,213 @@
 package org.buddycloud.channelserver.packetprocessor.iq.namespace.search;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.concurrent.BlockingQueue;
 
 import org.buddycloud.channelserver.channel.ChannelManager;
 import org.buddycloud.channelserver.packetprocessor.PacketProcessor;
+import org.dom4j.Element;
+import org.xmpp.forms.DataForm;
+import org.xmpp.forms.FormField;
 import org.xmpp.packet.IQ;
 import org.xmpp.packet.Packet;
-
+import org.xmpp.packet.PacketError;
 
 public class SearchSet implements PacketProcessor<IQ> {
 
 	private ChannelManager channelManager;
 	private BlockingQueue<Packet> outQueue;
+	private IQ responseIq;
+	private Element x;
+	private Element query;
+	private IQ requestIq;
+	private ArrayList<String> content;
+	private String author;
+	private int page = 1;
+	private int rpp = 25;
 
-	public SearchSet(BlockingQueue<Packet> outQueue, ChannelManager channelManager) {
+	public SearchSet(BlockingQueue<Packet> outQueue,
+			ChannelManager channelManager) {
 		this.channelManager = channelManager;
 		this.outQueue = outQueue;
 	}
-	
-	@Override
-	public void process(IQ packet) throws Exception {
 
+	@Override
+	public void process(IQ request) throws Exception {
+		responseIq = IQ.createResultIQ(request);
+		this.requestIq = request;
+
+		if (false == isValidRequest()) {
+			return;
+		}
+
+		if ( false == processForm() ) {
+			return;
+		}
+
+		query = responseIq.getElement().addElement("query");
+		query.addAttribute("xmlns", Search.NAMESPACE_URI);
+
+		outQueue.put(responseIq);
 	}
+
+	private boolean isValidRequest() throws Exception {
+
+		if (false == channelManager.isLocalJID(requestIq.getFrom())) {
+			sendErrorResponse(PacketError.Type.cancel,
+					PacketError.Condition.not_allowed);
+			return false;
+		}
+
+		if (false == hasDataForm() || false == dataFormCorrect()) {
+			return false;
+		}
+
+		return true;
+	}
+
+	private boolean hasDataForm() throws Exception {
+		x = requestIq.getElement().element("query").element("x");
+
+		if (null == x || !DataForm.NAMESPACE.equals(x.attributeValue("xmlns"))
+				|| !"submit".equals(x.attributeValue("type"))) {
+			sendErrorResponse(PacketError.Type.modify,
+					PacketError.Condition.bad_request);
+			return false;
+		}
+
+		return true;
+	}
+
+	private boolean dataFormCorrect() throws Exception {
+		if (!hasCorrectFormElement() || !hasEnoughFormFields()) {
+
+			sendErrorResponse(PacketError.Type.modify,
+					PacketError.Condition.bad_request);
+			return false;
+		}
+
+		return true;
+	}
+
+	private boolean hasCorrectFormElement() throws Exception {
+
+		List<Element> elements = x.elements("field");
+
+		if (elements.size() > 0) {
+			for (Element field : elements) {
+				if (!"FORM_TYPE".equals(field.attributeValue("var"))) {
+					continue;
+				}
+
+				String value = field.elementText("value");
+
+				if (null == value || !DataForm.NAMESPACE.equals(value)) {
+					return false;
+				}
+
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	private boolean hasEnoughFormFields() throws Exception {
+		List<Element> elements = x.elements("field");
+		if (elements.size() < 2) {
+			return false;
+		}
+
+		boolean hasContentOrAuthor = false;
+
+		String var;
+		for (Element field : elements) {
+			var = field.attributeValue("var");
+			if ("content".equals(var) || "author".equals(var)) {
+				hasContentOrAuthor = true;
+			}
+		}
+
+		return hasContentOrAuthor;
+	}
+
+	private boolean processForm() throws Exception {
+		try {
+			extractFieldValues();
+		} catch ( NumberFormatException e ) {
+			return false;
+		}
+		
+		if ( false == checkFieldValues() ) {
+			return false;
+		}
+		
+		return true;
+	}
+	
+	private void extractFieldValues() {
+		List<Element> elements = x.elements("field");
+		String var;
+		for (Element field : elements) {
+			var = field.attributeValue("var");
+			if ( "content".equals(var) ) {
+				content = getValuesAsList( field );
+			} else if ( "author".equals(var) ) {
+				author = field.elementText("value");
+			} else if ( "page".equals(var) ) {
+				page = getValueAsNumber( field );
+			} else if ( "rpp".equals(var) ) {
+				rpp = getValueAsNumber( field );
+			}
+		}
+	}
+	
+	private boolean checkFieldValues() throws Exception {
+		if ( ( null != content && content.size() > 0 ) || 
+			 ( null != author && author.length() > 0 ) ) {
+			return true;
+		}
+
+		sendErrorResponse(PacketError.Type.modify,
+				PacketError.Condition.bad_request);
+		return false;
+		
+	}
+	
+	private ArrayList<String> getValuesAsList( Element field ) {
+		ArrayList<String> rtn = new ArrayList<String>();
+		String valueText;
+		for ( Element value : (List<Element>) field.elements("value") ) {
+			valueText = value.getText();
+			if ( valueText.length() == 0 ) {
+				continue;
+			}
+			rtn.add( valueText );
+		}
+		return rtn;
+	}
+	
+	private Integer getValueAsNumber( Element field ) throws NumberFormatException {
+		String valueStr = field.elementText("value");
+		return Integer.parseInt(valueStr);
+	}
+
+	private void sendErrorResponse(PacketError.Type type,
+			PacketError.Condition condition) throws InterruptedException {
+		responseIq.setType(IQ.Type.error);
+		PacketError error = new PacketError(condition, type);
+		responseIq.setError(error);
+
+		outQueue.put(responseIq);
+	}
+
+	// private void processForm() { // throws Something
+	// Element element;
+	// DataForm dataForm = new DataForm(element);
+	// List<FormField> fields = dataForm.getFields();
+	// // do something with fields;
+	// }
 
 }
--- src/test/java/org/buddycloud/channelserver/packetprocessor/iq/namespace/search/SearchSetTest.java
+++ src/test/java/org/buddycloud/channelserver/packetprocessor/iq/namespace/search/SearchSetTest.java
@@ -35,11 +35,13 @@ import org.dom4j.tree.BaseElement;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
+import org.xmpp.forms.DataForm;
 import org.xmpp.packet.IQ;
 import org.xmpp.packet.JID;
 import org.xmpp.packet.Packet;
 import org.xmpp.packet.PacketError;
 import org.xmpp.resultsetmanagement.ResultSetImpl;
+
 
 public class SearchSetTest extends IQTestHandler {
 
@@ -50,6 +52,9 @@ public class SearchSetTest extends IQTestHandler {
 	private ChannelManager channelManager;
 
 	private SearchSet search;
+	private JID sender;
+	private JID receiver;
+
 
 	@Before
 	public void setUp() throws Exception {
@@ -59,11 +64,233 @@ public class SearchSetTest extends IQTestHandler {
 
 		search = new SearchSet(queue, channelManager);
 
+		sender = new JID("channels.shakespeare.lit");
+		receiver = new JID("romeo@shakespeare.lit/home");
+		
+		request = new IQ();
+		request.setFrom(receiver);
+		request.setType(IQ.Type.set);
+		request.setTo(sender);
+		Element query = request.getElement().addElement("query");
+		query.addNamespace("", Search.NAMESPACE_URI);
+
+		Mockito.when(channelManager.isLocalJID(Mockito.any(JID.class)))
+				.thenReturn(true);
 	}
 
 	@Test
-	public void testTrueIsTrue() {
-		Assert.assertTrue(true);
-	}
+	public void testOnlyAcceptsPacketsFromLocalUsers() throws Exception {
 
+		Mockito.when(channelManager.isLocalJID(Mockito.any(JID.class)))
+				.thenReturn(false);
+
+		search.process(request);
+		Packet response = queue.poll();
+		PacketError error = response.getError();
+		Assert.assertNotNull(error);
+		Assert.assertEquals(PacketError.Type.cancel, error.getType());
+		Assert.assertEquals(PacketError.Condition.not_allowed,
+				error.getCondition());
+	}
+		
+	@Test
+	public void testReturnsErrorIfDataFormAbsent() throws Exception {
+
+		search.process(request);
+		Packet response = queue.poll();
+		PacketError error = response.getError();
+		Assert.assertNotNull(error);
+		Assert.assertEquals(PacketError.Type.modify, error.getType());
+		Assert.assertEquals(PacketError.Condition.bad_request,
+				error.getCondition());
+	}
+	
+	@Test
+	public void testReturnsErrorIfNamespaceIncorrect() throws Exception {
+		Element query = request.getElement().element("query");
+		Element x = query.addElement("x");
+		x.addAttribute("xmlns", "some:other:namespace");
+
+		search.process(request);
+		Packet response = queue.poll();
+		PacketError error = response.getError();
+		Assert.assertNotNull(error);
+		Assert.assertEquals(PacketError.Type.modify, error.getType());
+		Assert.assertEquals(PacketError.Condition.bad_request,
+				error.getCondition());
+	}
+	
+	@Test
+	public void testReturnsErrorIfTypeIncorrect() throws Exception {
+		Element query = request.getElement().element("query");
+		Element x = query.addElement("x");
+		x.addAttribute("xmlns", DataForm.NAMESPACE);
+		x.addAttribute("type", "wrongtype");
+
+		search.process(request);
+		Packet response = queue.poll();
+		PacketError error = response.getError();
+		Assert.assertNotNull(error);
+		Assert.assertEquals(PacketError.Type.modify, error.getType());
+		Assert.assertEquals(PacketError.Condition.bad_request,
+				error.getCondition());
+	}
+	
+	@Test
+	public void testReturnsErrorIfFieldFormTypeIsIncorrect() throws Exception {
+		Element query = request.getElement().element("query");
+		Element x = query.addElement("x");
+		x.addAttribute("xmlns", DataForm.NAMESPACE);
+		x.addAttribute("type", "submit");
+		
+		Element field = x.addElement("field");
+		field.addAttribute("var", "NOT_FORM_TYPE");
+
+		search.process(request);
+		Packet response = queue.poll();
+		PacketError error = response.getError();
+		Assert.assertNotNull(error);
+		Assert.assertEquals(PacketError.Type.modify, error.getType());
+		Assert.assertEquals(PacketError.Condition.bad_request,
+				error.getCondition());
+	}
+	
+	@Test
+	public void testReturnsErrorIfFieldFormTypeValueIsIncorrect() throws Exception {
+		Element query = request.getElement().element("query");
+		Element x = query.addElement("x");
+		x.addAttribute("xmlns", DataForm.NAMESPACE);
+		x.addAttribute("type", "submit");
+		
+		Element field = x.addElement("field");
+		field.addAttribute("var", "FORM_TYPE");
+		field.addElement("value").addText("not:search:type");
+
+		search.process(request);
+		Packet response = queue.poll();
+		PacketError error = response.getError();
+		Assert.assertNotNull(error);
+		Assert.assertEquals(PacketError.Type.modify, error.getType());
+		Assert.assertEquals(PacketError.Condition.bad_request,
+				error.getCondition());
+	}
+	
+	@Test
+	public void testReturnsErrorIfTooFewFields() throws Exception {
+		Element query = request.getElement().element("query");
+		Element x = query.addElement("x");
+		x.addAttribute("xmlns", DataForm.NAMESPACE);
+		x.addAttribute("type", "submit");
+		
+		Element field = x.addElement("field");
+		field.addAttribute("var", "FORM_TYPE");
+		field.addElement("value").addText(DataForm.NAMESPACE);
+		
+		Element singleField = x.addElement("field");
+		singleField.addAttribute("var", "page");
+
+		search.process(request);
+		Packet response = queue.poll();
+		PacketError error = response.getError();
+		Assert.assertNotNull(error);
+		Assert.assertEquals(PacketError.Type.modify, error.getType());
+		Assert.assertEquals(PacketError.Condition.bad_request,
+				error.getCondition());
+	}
+	
+	@Test
+	public void testReturnsErrorIfContentValueIncorrect() throws Exception {
+		Element query = request.getElement().element("query");
+		Element x = query.addElement("x");
+		x.addAttribute("xmlns", DataForm.NAMESPACE);
+		x.addAttribute("type", "submit");
+		
+		Element field = x.addElement("field");
+		field.addAttribute("var", "FORM_TYPE");
+		field.addElement("value").addText(DataForm.NAMESPACE);
+		
+		Element singleField = x.addElement("field");
+		singleField.addAttribute("var", "content");
+		singleField.addElement("value");
+
+		search.process(request);
+		Packet response = queue.poll();
+		PacketError error = response.getError();
+		Assert.assertNotNull(error);
+		Assert.assertEquals(PacketError.Type.modify, error.getType());
+		Assert.assertEquals(PacketError.Condition.bad_request,
+				error.getCondition());
+	}
+	
+	@Test
+	public void testReturnsErrorIfAuthorValueIncorrect() throws Exception {
+		Element query = request.getElement().element("query");
+		Element x = query.addElement("x");
+		x.addAttribute("xmlns", DataForm.NAMESPACE);
+		x.addAttribute("type", "submit");
+		
+		Element field = x.addElement("field");
+		field.addAttribute("var", "FORM_TYPE");
+		field.addElement("value").addText(DataForm.NAMESPACE);
+		
+		Element singleField = x.addElement("field");
+		singleField.addAttribute("var", "author");
+		singleField.addElement("value");
+
+		search.process(request);
+		Packet response = queue.poll();
+		PacketError error = response.getError();
+		Assert.assertNotNull(error);
+		Assert.assertEquals(PacketError.Type.modify, error.getType());
+		Assert.assertEquals(PacketError.Condition.bad_request,
+				error.getCondition());
+	}
+	
+	@Test
+	public void testReturnsErrorIfPageValueIncorrect() throws Exception {
+		Element query = request.getElement().element("query");
+		Element x = query.addElement("x");
+		x.addAttribute("xmlns", DataForm.NAMESPACE);
+		x.addAttribute("type", "submit");
+		
+		Element field = x.addElement("field");
+		field.addAttribute("var", "FORM_TYPE");
+		field.addElement("value").addText(DataForm.NAMESPACE);
+		
+		Element singleField = x.addElement("field");
+		singleField.addAttribute("var", "page");
+		singleField.addElement("value").setText("sausages");
+
+		search.process(request);
+		Packet response = queue.poll();
+		PacketError error = response.getError();
+		Assert.assertNotNull(error);
+		Assert.assertEquals(PacketError.Type.modify, error.getType());
+		Assert.assertEquals(PacketError.Condition.bad_request,
+				error.getCondition());
+	}
+	
+	@Test
+	public void testReturnsErrorIfRppValueIncorrect() throws Exception {
+		Element query = request.getElement().element("query");
+		Element x = query.addElement("x");
+		x.addAttribute("xmlns", DataForm.NAMESPACE);
+		x.addAttribute("type", "submit");
+		
+		Element field = x.addElement("field");
+		field.addAttribute("var", "FORM_TYPE");
+		field.addElement("value").addText(DataForm.NAMESPACE);
+		
+		Element singleField = x.addElement("field");
+		singleField.addAttribute("var", "rpp");
+		singleField.addElement("value").setText("bananas");
+
+		search.process(request);
+		Packet response = queue.poll();
+		PacketError error = response.getError();
+		Assert.assertNotNull(error);
+		Assert.assertEquals(PacketError.Type.modify, error.getType());
+		Assert.assertEquals(PacketError.Condition.bad_request,
+				error.getCondition());
+	}
 }

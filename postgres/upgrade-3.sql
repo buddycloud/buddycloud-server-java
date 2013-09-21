b711701cadfd8c94d27f657d6d5773a1c89a3efa
Simon Tennant <simon@buddycloud.com>
2013-09-21 16:32:10 +0200

    adding more NOT NULL constraints to the subscriptions table

--- postgres/upgrade-3.sql
+++ postgres/upgrade-3.sql
@@ -2,7 +2,13 @@ BEGIN TRANSACTION;
 
 -- Ensure there really are listeners
 
+ALTER TABLE subscriptions ALTER COLUMN node set NOT NULL;
+ALTER TABLE subscriptions ALTER COLUMN "user" set NOT NULL;
 ALTER TABLE subscriptions ALTER COLUMN listener set NOT NULL;
+ALTER TABLE subscriptions ALTER COLUMN subscription set NOT NULL;
+ALTER TABLE subscriptions ALTER COLUMN updated set NOT NULL;
+ALTER TABLE subscriptions ALTER COLUMN anonymous set NOT NULL;
+ALTER TABLE subscriptions ALTER COLUMN temporary set NOT NULL;
 
 INSERT INTO schema_version (version, "when", description)
        VALUES (3, 'now', 'Added listeners constraint to subscriptions table');

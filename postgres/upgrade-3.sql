6b0bf9bbce8829372432c09f6d7e71c907337fe8
Simon Tennant <simon@buddycloud.com>
2013-09-21 16:41:23 +0200

    Java server doesn't update anonymous and temporary subscription columns
    channel-server=# ALTER TABLE subscriptions ALTER COLUMN temporary DROP NOT NULL ; 
    ALTER TABLE
    channel-server=# ALTER TABLE subscriptions ALTER COLUMN anonymous  DROP NOT NULL ; 
    ALTER TABLE

--- postgres/upgrade-3.sql
+++ postgres/upgrade-3.sql
@@ -7,8 +7,6 @@ ALTER TABLE subscriptions ALTER COLUMN "user" set NOT NULL;
 ALTER TABLE subscriptions ALTER COLUMN listener set NOT NULL;
 ALTER TABLE subscriptions ALTER COLUMN subscription set NOT NULL;
 ALTER TABLE subscriptions ALTER COLUMN updated set NOT NULL;
-ALTER TABLE subscriptions ALTER COLUMN anonymous set NOT NULL;
-ALTER TABLE subscriptions ALTER COLUMN temporary set NOT NULL;
 
 INSERT INTO schema_version (version, "when", description)
        VALUES (3, 'now', 'Added listeners constraint to subscriptions table');

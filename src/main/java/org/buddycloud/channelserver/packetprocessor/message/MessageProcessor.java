842ed706f6e0e78db39a38eacd623eef52634d78
abmargb <abmargb@gmail.com>
2013-10-10 18:33:47 +0200

    Restoring MessageProcessor for node deletion.

--- src/main/java/org/buddycloud/channelserver/packetprocessor/message/MessageProcessor.java
+++ src/main/java/org/buddycloud/channelserver/packetprocessor/message/MessageProcessor.java
@@ -9,6 +9,7 @@ import org.buddycloud.channelserver.packetprocessor.PacketProcessor;
 import org.buddycloud.channelserver.packetprocessor.message.event.AbstractMessageProcessor;
 import org.buddycloud.channelserver.packetprocessor.message.event.AffiliationProcessor;
 import org.buddycloud.channelserver.packetprocessor.message.event.ConfigurationProcessor;
+import org.buddycloud.channelserver.packetprocessor.message.event.DeleteProcessor;
 import org.buddycloud.channelserver.packetprocessor.message.event.ItemsProcessor;
 import org.buddycloud.channelserver.packetprocessor.message.event.RetractItemProcessor;
 import org.buddycloud.channelserver.packetprocessor.message.event.SubscriptionProcessor;
@@ -32,6 +33,7 @@ public class MessageProcessor implements PacketProcessor<Message> {
 	public static final String SUBSCRIPTION = "subscription";
 	private static final String AFFILIATIONS = "affiliations";
 	private static final String CONFIGURATION = "configuration";
+	private static final String DELETE = "delete";
 
 	public MessageProcessor(BlockingQueue<Packet> outQueue, Properties conf,
 			ChannelManager channelManager) {
@@ -70,6 +72,9 @@ public class MessageProcessor implements PacketProcessor<Message> {
 					channelManager);
 		} else if (true == name.equals(CONFIGURATION)) {
 			handler = new ConfigurationProcessor(outQueue, configuration,
+					channelManager);
+		} else if (true == name.equals(DELETE)) {
+			handler = new DeleteProcessor(outQueue, configuration, 
 					channelManager);
 		}
 		if (null == handler) {
---
+++ src/main/java/org/buddycloud/channelserver/packetprocessor/message/event/DeleteProcessor.java
@@ -0,0 +1,40 @@
+package org.buddycloud.channelserver.packetprocessor.message.event;
+
+import java.util.Properties;
+import java.util.concurrent.BlockingQueue;
+
+import org.buddycloud.channelserver.channel.ChannelManager;
+import org.buddycloud.channelserver.db.exception.NodeStoreException;
+import org.dom4j.Element;
+import org.xmpp.packet.Message;
+import org.xmpp.packet.Packet;
+ 
+public class DeleteProcessor extends AbstractMessageProcessor  {
+ 
+	public DeleteProcessor(BlockingQueue<Packet> outQueue,
+			Properties configuration, ChannelManager channelManager) {
+		super(channelManager, configuration, outQueue);
+	}
+
+	@Override
+	public void process(Message packet) throws Exception {
+		message = packet;
+		deleteNode();
+		if (!channelManager.isLocalNode(node)) {
+			sendLocalNotifications();
+		}
+	}
+
+	private void deleteNode() throws NodeStoreException {
+		Element deleteElement = message.getElement().element("event")
+				.element("delete");
+		if (deleteElement == null) {
+			return;
+		}
+		node = deleteElement.attributeValue("node");
+		if (channelManager.isLocalNode(node)) {
+			return;
+		}
+        channelManager.deleteNode(node);
+	}
+}

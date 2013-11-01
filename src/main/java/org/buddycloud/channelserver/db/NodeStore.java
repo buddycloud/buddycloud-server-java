f19c0d64198d797013e82920f2116cba722f9756
Lloyd Watkin <lloyd.watkin@surevine.com>
2013-11-01 15:34:59 +0000

    Adding comments

--- src/main/java/org/buddycloud/channelserver/db/NodeStore.java
+++ src/main/java/org/buddycloud/channelserver/db/NodeStore.java
@@ -573,16 +573,57 @@ public interface NodeStore {
 	 */
 	int getFirehoseItemCount(boolean isAdmin) throws NodeStoreException;
 	
+	/**
+	 * Get a list of posts for a user
+	 * 
+	 * @param userJid
+	 * @return
+	 * @throws NodeStoreException
+	 */
 	ResultSet<NodeItem> getUserItems(JID userJid) throws NodeStoreException;
 	
+	/**
+	 * Delete user posts
+	 * 
+	 * @param userJid
+	 * @throws NodeStoreException
+	 */
 	void deleteUserItems(JID userJid) throws NodeStoreException;
 	
+	/**
+	 * Delete affiliations for a user
+	 * 
+	 * @param userJid
+	 * @throws NodeStoreException
+	 */
 	void deleteUserAffiliations(JID userJid) throws NodeStoreException;
 	
+	/**
+	 * Delete user subscriptions
+	 * 
+	 * @param userJid
+	 * @throws NodeStoreException
+	 */
 	void deleteUserSubscriptions(JID userJid) throws NodeStoreException;
 	
+	/**
+	 * Get node threads
+	 * 
+	 * @param node
+	 * @param afterId
+	 * @param limit
+	 * @return
+	 * @throws NodeStoreException
+	 */
 	ResultSet<NodeThread> getNodeThreads(String node, String afterId, int limit) throws NodeStoreException;
 	
+	/**
+	 * Count node threads
+	 * 
+	 * @param node
+	 * @return
+	 * @throws NodeStoreException
+	 */
 	int countNodeThreads(String node) throws NodeStoreException;
 	
 	/**

c0d3135f96237143b3cda3c6735ad0b06dcfd102
Lloyd Watkin <lloyd.watkin@surevine.com>
2014-01-08 14:55:33 +0000

    Move some methods into TestHandler

--- src/test/java/org/buddycloud/channelserver/packetHandler/iq/TestHandler.java
+++ src/test/java/org/buddycloud/channelserver/packetHandler/iq/TestHandler.java
@@ -1,6 +1,45 @@
 package org.buddycloud.channelserver.packetHandler.iq;
 
-import junit.framework.Assert;
-import junit.framework.TestCase;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.StringReader;
 
-public class TestHandler {}
+import org.apache.commons.io.IOUtils;
+import org.dom4j.DocumentException;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
+import org.xmpp.packet.IQ;
+
+public class TestHandler {
+	
+    public final static String  STANZA_PATH = "src/test/resources/stanzas";
+    
+    public static String readStanzaAsString(String stanzaPath) 
+        throws IOException, DocumentException
+    {
+        String stanzaStr = IOUtils.toString(
+                new FileInputStream(STANZA_PATH + stanzaPath));
+        return stanzaStr.replaceAll("   ", "").replaceAll("\n", "");
+    }
+    
+    public static IQ readStanzaAsIq(String stanzaPath)
+        throws IOException, DocumentException
+    {
+        String stanzaStr = IOUtils.toString(
+                new FileInputStream(STANZA_PATH + stanzaPath));
+        return toIq(stanzaStr);
+    }
+
+    public static IQ toIq(String stanzaStr) throws DocumentException
+    {
+        return new IQ(parseXml(stanzaStr));
+    }
+    
+    public static Element parseXml(String stanzaStr) throws DocumentException {
+    	SAXReader xmlReader = new SAXReader();
+        xmlReader.setMergeAdjacentText(true);
+        xmlReader.setStringInternEnabled(true);
+        xmlReader.setStripWhitespaceText(true);
+        return xmlReader.read(new StringReader(stanzaStr)).getRootElement();
+	}
+}

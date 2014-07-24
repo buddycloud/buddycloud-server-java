eb296eaece56ef84b50e1daf81d785a338ac55b8
Lloyd Watkin <lloyd.watkin@surevine.com>
2014-07-24 15:35:54 +0100

    Truncate channel title to a max length of 128 characters

--- src/main/java/org/buddycloud/channelserver/channel/node/configuration/field/ChannelTitle.java
+++ src/main/java/org/buddycloud/channelserver/channel/node/configuration/field/ChannelTitle.java
@@ -4,11 +4,20 @@ public class ChannelTitle extends Field
 {
 	public static final String FIELD_NAME    = "pubsub#title";
 	public static final String DEFAULT_VALUE = "buddycloud channel title";
+	public static final int MAX_TITLE_LENGTH = 128;
 
 	public ChannelTitle()
 	{
 		name = FIELD_NAME;
 	}
+	
+    public String getValue()
+    {
+    	if (this.value.length() > MAX_TITLE_LENGTH) {
+    		this.value = this.value.substring(0, MAX_TITLE_LENGTH);
+    	}
+    	return this.value;
+    }
 	
 	public boolean isValid()
 	{
---
+++ src/test/java/org/buddycloud/channelserver/channel/node/configuration/field/ChannelTitleTest.java
@@ -0,0 +1,37 @@
+package org.buddycloud.channelserver.channel.node.configuration.field;
+
+import junit.framework.Assert;
+
+import org.apache.commons.lang.StringUtils;
+import org.buddycloud.channelserver.channel.node.configuration.field.ChannelTitle;
+import org.buddycloud.channelserver.db.exception.NodeStoreException;
+import org.buddycloud.channelserver.packetHandler.iq.IQTestHandler;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ChannelTitleTest extends IQTestHandler {
+	
+	private ChannelTitle field;
+
+	@Before
+	public void setUp() {
+		field = new ChannelTitle();
+
+	}
+
+	@Test
+	public void longestTitleIs128Characters()
+			throws NodeStoreException {
+		
+		String testTitle = StringUtils.repeat("Doc, you built a time machine? Out of a delorean?!", ChannelTitle.MAX_TITLE_LENGTH * 3);
+		field.setValue(testTitle);
+		Assert.assertEquals(field.MAX_TITLE_LENGTH, field.getValue().length());
+	}
+	
+	@Test
+	public void doesNotTrucateShorterStrings() {
+		String testTitle = "Hi, my name's George, George McFly";
+		field.setValue(testTitle);
+		Assert.assertEquals(testTitle, field.getValue());
+	}
+}

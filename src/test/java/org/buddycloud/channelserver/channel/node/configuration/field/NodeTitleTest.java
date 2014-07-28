e6f06f6366f2d958b76b1d625753f63703f3a846
Lloyd Watkin <lloyd.watkin@surevine.com>
2014-07-28 08:58:08 +0100

    Channel(Title/Description) -> Node(Title/Description)

--- src/test/java/org/buddycloud/channelserver/channel/node/configuration/field/NodeTitleTest.java
+++ src/test/java/org/buddycloud/channelserver/channel/node/configuration/field/NodeTitleTest.java
@@ -3,27 +3,26 @@ package org.buddycloud.channelserver.channel.node.configuration.field;
 import junit.framework.Assert;
 
 import org.apache.commons.lang.StringUtils;
-import org.buddycloud.channelserver.channel.node.configuration.field.ChannelTitle;
+import org.buddycloud.channelserver.channel.node.configuration.field.NodeTitle;
 import org.buddycloud.channelserver.db.exception.NodeStoreException;
 import org.buddycloud.channelserver.packetHandler.iq.IQTestHandler;
 import org.junit.Before;
 import org.junit.Test;
 
-public class ChannelTitleTest extends IQTestHandler {
+public class NodeTitleTest extends IQTestHandler {
 	
-	private ChannelTitle field;
+	private NodeTitle field;
 
 	@Before
 	public void setUp() {
-		field = new ChannelTitle();
-
+		field = new NodeTitle();
 	}
 
 	@Test
 	public void longestTitleIs128Characters()
 			throws NodeStoreException {
 		
-		String testTitle = StringUtils.repeat("Doc, you built a time machine? Out of a delorean?!", ChannelTitle.MAX_TITLE_LENGTH);
+		String testTitle = StringUtils.repeat("Doc, you built a time machine? Out of a delorean?!", NodeTitle.MAX_TITLE_LENGTH);
 		field.setValue(testTitle);
 		Assert.assertEquals(field.MAX_TITLE_LENGTH, field.getValue().length());
 	}

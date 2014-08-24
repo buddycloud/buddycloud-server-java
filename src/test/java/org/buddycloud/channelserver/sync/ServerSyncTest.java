6e4aee85f5a51ea141f081508ad8dc78604e889c
Lloyd Watkin <lloyd.watkin@surevine.com>
2014-08-24 19:11:45 +0100

    Prevent remote data purge on server start

--- src/test/java/org/buddycloud/channelserver/sync/ServerSyncTest.java
+++ src/test/java/org/buddycloud/channelserver/sync/ServerSyncTest.java
@@ -3,6 +3,8 @@ package org.buddycloud.channelserver.sync;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
+import org.buddycloud.channelserver.Configuration;
+import org.buddycloud.channelserver.channel.ChannelManager;
 import org.buddycloud.channelserver.channel.ChannelManagerFactory;
 import org.buddycloud.channelserver.packetHandler.iq.IQTestHandler;
 import org.junit.Before;
@@ -15,18 +17,60 @@ public class ServerSyncTest extends IQTestHandler {
 	private BlockingQueue<Packet> outQueue = new LinkedBlockingQueue<Packet>();
 	private BlockingQueue<Packet> inQueue = new LinkedBlockingQueue<Packet>();
 	private ChannelManagerFactory channelManagerFactory;
+	private ChannelManager channelManager;
 	private ServerSync serverSync;
+	private Configuration configuration;
 
 	@Before
 	public void setUp() throws Exception {
 
+		configuration = Mockito.mock(Configuration.class);
+
+		channelManager = Mockito.mock(ChannelManager.class);
 		channelManagerFactory = Mockito.mock(ChannelManagerFactory.class);
-		serverSync = new ServerSync(channelManagerFactory, outQueue, inQueue);
+		Mockito.when(channelManagerFactory.create()).thenReturn(channelManager);
+		serverSync = new ServerSync(channelManagerFactory, outQueue, inQueue,
+				configuration);
 	}
 
 	@Test
 	public void testChannelManagerIsCreated() {
-		
+
 		Mockito.verify(channelManagerFactory, Mockito.times(1)).create();
 	}
+
+	@Test
+	public void ifUserChoosesToPurgeRemoteDataThenMethodIsCalled()
+			throws Exception {
+
+		Mockito.when(
+				configuration.getProperty(
+						Mockito.eq(Configuration.PURGE_REMOTE_ON_START),
+						Mockito.eq("false"))).thenReturn("true");
+		serverSync.start();
+		Mockito.verify(channelManager, Mockito.times(1)).deleteRemoteData();
+	}
+
+	@Test
+	public void ifUserChoosesNotToPurgeRemoteDataThenMethodIsNotCalled()
+			throws Exception {
+		Mockito.when(
+				configuration.getProperty(
+						Mockito.eq(Configuration.PURGE_REMOTE_ON_START),
+						Mockito.eq("false"))).thenReturn("false");
+		serverSync.start();
+		Mockito.verify(channelManager, Mockito.times(0)).deleteRemoteData();
+	}
+
+	@Test
+	public void ifNoChoiceIsMadeAboutPurgingRemoteDataThenMethodIsNotCalled()
+			throws Exception {
+		Mockito.when(
+				configuration.getProperty(
+						Mockito.eq(Configuration.PURGE_REMOTE_ON_START),
+						Mockito.eq("false"))).thenReturn(null);
+		serverSync.start();
+		Mockito.verify(channelManager, Mockito.times(0)).deleteRemoteData();
+	}
+
 }

b3766d951e4e574938c84f8dbd3b1d6b47739c86
Lloyd Watkin <lloyd.watkin@surevine.com>
2014-10-14 14:32:38 +0100

    Increase coverage of NodeAffiliationImpl to 100%

--- src/test/java/org/buddycloud/channelserver/pubsub/model/impl/NodeAffiliationImplTest.java
+++ src/test/java/org/buddycloud/channelserver/pubsub/model/impl/NodeAffiliationImplTest.java
@@ -1,14 +1,29 @@
 package org.buddycloud.channelserver.pubsub.model.impl;
 
+import java.util.Date;
+
+import junit.framework.Assert;
 import nl.jqno.equalsverifier.EqualsVerifier;
 
+import org.buddycloud.channelserver.pubsub.affiliation.Affiliations;
 import org.junit.Test;
+import org.xmpp.packet.JID;
 
 public class NodeAffiliationImplTest {
 
-    @Test
-    public void testEquals() {
-        EqualsVerifier.forClass(NodeAffiliationImpl.class).verify();
-    }
+  private String node = "/user/test@example.com/posts";
+  private JID fullJid = new JID("test@example.com/resource");
+
+  @Test
+  public void testEquals() {
+    EqualsVerifier.forClass(NodeAffiliationImpl.class).verify();
+  }
+
+  @Test
+  public void providingFullJidSetsAsAsBareJid() throws Exception {
+    NodeAffiliationImpl affiliation =
+        new NodeAffiliationImpl(node, fullJid, Affiliations.member, new Date());
+    Assert.assertEquals(fullJid.toBareJID(), affiliation.getUser().toString());
+  }
 
 }

b7ae491581f09b1891083522d71b927fbdd0414b
Lloyd Watkin <lloyd.watkin@surevine.com>
2014-11-04 08:50:53 +0000

    Wait for component connections

--- src/main/java/org/buddycloud/channelserver/Main.java
+++ src/main/java/org/buddycloud/channelserver/Main.java
@@ -2,49 +2,80 @@ package org.buddycloud.channelserver;
 
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
+import org.buddycloud.channelserver.db.exception.NodeStoreException;
+import org.logicalcobwebs.proxool.ProxoolException;
+import org.xmpp.component.ComponentException;
 
 public class Main {
 
-    private static final Logger LOG = Logger.getLogger(Main.class);
+  private static final Logger LOG = Logger.getLogger(Main.class);
+  private static Configuration configuration;
 
-    public static void main(String[] args) {
-        try {
-            startComponents();
-        } catch (Exception e) {
-            LOG.error("Failed during initialization.", e);
-        }
+  public static void main(String[] args) {
+    try {
+      startComponents();
+    } catch (Exception e) {
+      LOG.error("Failed during initialization.", e);
     }
+  }
 
-    private static void startComponents() throws Exception {
-        PropertyConfigurator.configure("log4j.properties");
-        LOG.info("Starting Buddycloud channel mockup version...");
+  private static void startComponents() throws Exception {
+    PropertyConfigurator.configure("log4j.properties");
+    LOG.info("Starting Buddycloud channel mockup version...");
 
-        Configuration configuration = Configuration.getInstance();
+    configuration = Configuration.getInstance();
 
-        LOG.info("Connecting to '" + configuration.getXmppHost() + ":" + configuration.getComponentPort() + "' and trying to claim address '"
-                + configuration.getProperty("server.domain") + "'.");
+    LOG.info("Connecting to '" + configuration.getXmppHost() + ":"
+        + configuration.getComponentPort() + "' and trying to claim address '"
+        + configuration.getProperty("server.domain") + "'.");
 
-        String channelDomain = configuration.getProperty(Configuration.CONFIGURATION_SERVER_CHANNELS_DOMAIN);
-        String topicDomain = configuration.getProperty(Configuration.CONFIGURATION_SERVER_TOPICS_DOMAIN);
-
-        if (channelDomain == null) {
-            throw new IllegalArgumentException("Property server.domain.channels is mandatory.");
-        }
-        new XmppComponent(configuration, channelDomain).run();
-
-        if (topicDomain != null) {
-            new TopicsComponent(configuration, topicDomain).run();
-        }
-        hang();
+    while (false == startChannelComponent()) {
+      Thread.sleep(5000);
+      LOG.info("Waiting for component connection");
     }
-
-    private static void hang() {
-        while (true) {
-            try {
-                Thread.sleep(5000);
-            } catch (InterruptedException e) {
-                LOG.error(e);
-            }
-        }
+    while (false == startTopicComponent()) {
+      Thread.sleep(5000);
+      LOG.info("Waiting for topic component connection");
     }
+    hang();
+  }
+
+  private static boolean startTopicComponent() {
+    String topicDomain = configuration.getProperty(Configuration.CONFIGURATION_SERVER_TOPICS_DOMAIN);
+    if (topicDomain == null) {
+      return true;
+    }
+    try {
+      new TopicsComponent(configuration, topicDomain).run();
+    } catch (ComponentException e) {
+      return false;
+    }
+    return true;
+  }
+
+  private static boolean startChannelComponent() throws Exception {
+    String channelDomain =
+        configuration.getProperty(Configuration.CONFIGURATION_SERVER_CHANNELS_DOMAIN);
+    if (channelDomain == null) {
+      throw new IllegalArgumentException("Property server.domain.channels is mandatory.");
+    }
+    try {
+      new XmppComponent(configuration, channelDomain).run();
+    } catch (ComponentException e) {
+      return false;
+    } catch (ProxoolException e) {
+      throw new NodeStoreException(e);
+    }
+    return true;
+  }
+
+  private static void hang() {
+    while (true) {
+      try {
+        Thread.sleep(5000);
+      } catch (InterruptedException e) {
+        LOG.error(e);
+      }
+    }
+  }
 }

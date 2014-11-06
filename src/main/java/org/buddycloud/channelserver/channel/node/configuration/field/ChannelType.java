3443ed3e3478988d6ed46c455ab8acb63c7b91e3
Lloyd Watkin <lloyd.watkin@surevine.com>
2014-11-06 17:19:16 +0000

    Formatting update

--- src/main/java/org/buddycloud/channelserver/channel/node/configuration/field/ChannelType.java
+++ src/main/java/org/buddycloud/channelserver/channel/node/configuration/field/ChannelType.java
@@ -1,29 +1,29 @@
 package org.buddycloud.channelserver.channel.node.configuration.field;
 
 public class ChannelType extends Field {
-    public static final String FIELD_NAME = "buddycloud#channel_type";
-    public static final String DEFAULT_VALUE = ChannelType.models.PERSONAL.toString();
+  public static final String FIELD_NAME = "buddycloud#channel_type";
+  public static final String DEFAULT_VALUE = ChannelType.models.PERSONAL.toString();
 
-    public ChannelType() {
-        name = FIELD_NAME;
+  public ChannelType() {
+    name = FIELD_NAME;
+  }
+
+  public enum models {
+    PERSONAL("personal"), TOPIC("topic"), EPHEMERAL("ephemeral");
+    String model = null;
+
+    private models(String model) {
+      this.model = model;
     }
 
-    public enum models {
-        PERSONAL("personal"), TOPIC("topic"), EPHEMERAL("ephemeral");
-        String model = null;
-
-        private models(String model) {
-            this.model = model;
-        }
-
-        public String toString() {
-            return model;
-        }
+    public String toString() {
+      return model;
     }
+  }
 
-    public boolean isValid() {
-        return (getValue().equals(ChannelType.models.PERSONAL.toString()) ||
-            getValue().equals(ChannelType.models.TOPIC.toString()) ||
-            getValue().equals(ChannelType.models.EPHEMERAL.toString()));
-    }
+  public boolean isValid() {
+    return (getValue().equals(ChannelType.models.PERSONAL.toString())
+        || getValue().equals(ChannelType.models.TOPIC.toString()) || getValue().equals(
+        ChannelType.models.EPHEMERAL.toString()));
+  }
 }

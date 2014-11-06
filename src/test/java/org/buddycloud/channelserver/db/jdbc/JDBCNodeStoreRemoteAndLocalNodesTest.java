dd43617480135f0e89662d2cadd75302fc1b7c73
Lloyd Watkin <lloyd.watkin@surevine.com>
2014-11-06 18:23:02 +0000

    Don't advertise nodes we don't wish to advertise

--- src/test/java/org/buddycloud/channelserver/db/jdbc/JDBCNodeStoreRemoteAndLocalNodesTest.java
+++ src/test/java/org/buddycloud/channelserver/db/jdbc/JDBCNodeStoreRemoteAndLocalNodesTest.java
@@ -22,6 +22,7 @@ public class JDBCNodeStoreRemoteAndLocalNodesTest extends JDBCNodeStoreAbstract 
     public void setup() throws SQLException, IOException, NodeStoreException {
         dbTester.loadData("node_1");
         dbTester.loadData("node_2");
+        dbTester.loadData("advertise_nodes");
         store.addRemoteNode(TEST_SERVER2_NODE1_ID);
     }
 
@@ -31,6 +32,13 @@ public class JDBCNodeStoreRemoteAndLocalNodesTest extends JDBCNodeStoreAbstract 
         Assert.assertTrue(localNodes.contains(TEST_SERVER1_NODE1_ID));
         Assert.assertTrue(localNodes.contains(TEST_SERVER1_NODE2_ID));
         Assert.assertFalse(localNodes.contains(TEST_SERVER2_NODE1_ID));
+        Assert.assertTrue(localNodes.contains("/user/advertised@server1/posts"));
+        Assert.assertTrue(localNodes.contains("/user/undetermined-advertised@server1/posts"));
+        Assert.assertFalse(localNodes.contains("/user/not-advertised@server1/posts"));
+        Assert.assertFalse(localNodes.contains("/user/advertised@server2/posts"));
+        Assert.assertFalse(localNodes.contains("/user/undetermined-advertised@server2/posts"));
+        Assert.assertFalse(localNodes.contains("/user/not-advertised@server2/posts"));
+        Assert.assertEquals(4, localNodes.size());
     }
     
     @Test
@@ -39,5 +47,12 @@ public class JDBCNodeStoreRemoteAndLocalNodesTest extends JDBCNodeStoreAbstract 
         Assert.assertFalse(remoteNodes.contains(TEST_SERVER1_NODE1_ID));
         Assert.assertFalse(remoteNodes.contains(TEST_SERVER1_NODE2_ID));
         Assert.assertTrue(remoteNodes.contains(TEST_SERVER2_NODE1_ID));
+        Assert.assertFalse(remoteNodes.contains("/user/advertised@server1/posts"));
+        Assert.assertFalse(remoteNodes.contains("/user/undetermined-advertised@server1/posts"));
+        Assert.assertFalse(remoteNodes.contains("/user/not-advertised@server1/posts"));
+        Assert.assertTrue(remoteNodes.contains("/user/advertised@server2/posts"));
+        Assert.assertTrue(remoteNodes.contains("/user/undetermined-advertised@server2/posts"));
+        Assert.assertFalse(remoteNodes.contains("/user/not-advertised@server2/posts"));
+        Assert.assertEquals(3, remoteNodes.size());
     }
 }

46b7ec012737f4e7c22276d1af7111b3146917f4
abmargb <abmargb@gmail.com>
2014-11-30 14:31:49 -0300

    Fix for org.buddycloud.channelserver.MainTest

--- src/main/java/org/buddycloud/channelserver/TopicsComponent.java
+++ src/main/java/org/buddycloud/channelserver/TopicsComponent.java
@@ -1,13 +1,11 @@
 package org.buddycloud.channelserver;
 
 import org.buddycloud.channelserver.utils.ComponentBase;
-import org.jivesoftware.whack.ExternalComponentManager;
-import org.xmpp.component.ComponentException;
 
 public class TopicsComponent extends ComponentBase {
 
     public TopicsComponent(Configuration configuration, String domain) {
       initialize(configuration, domain);
-      new TopicsEngine(configuration);
+      this.engine = new TopicsEngine(configuration);
     }
 }
